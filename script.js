6ffea9039df478f57f2732e4be28c2e1783fe025
X474X <133019572+X474X@users.noreply.github.com>
2023-11-13 20:55:25 +0200

    Base design finished

---
+++ script.js
@@ -0,0 +1,38 @@
+// const x = [
+//   {
+//     image: "img",
+//     title: "Product1",
+//     rating: 5,
+//     price: 200,
+//   },
+//   {
+//     image: "img",
+//     title: "Product2",
+//     rating: 5,
+//     price: 200,
+//   },
+//   {
+//     image: "img",
+//     title: "Product3",
+//     rating: 5,
+//     price: 200,
+//   },
+//   {
+//     image: "img",
+//     title: "Product4",
+//     rating: 5,
+//     price: 200,
+//   },
+// ];
+
+// Sa se afiseze numele personelor care au varsta mai mare de 20 de ani,
+// in caz contrat numele lor o sa fie modificat cu Piticilor
+
+// for (let i = 0; i < x.length; i++) {
+//   if (x[i].varsta > 20) {
+//     console.log(x[i].nume);
+//   } else {
+//     x[i].nume = "Pitic";
+//     console.log(x[i].nume);
+//   }
+// }
